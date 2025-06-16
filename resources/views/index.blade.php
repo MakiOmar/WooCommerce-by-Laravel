4bc7bfd15b690b29cfdf37d23cfada6b3d7b0220
makiomar <mo7amed.maki@gmail.com>
2025-06-16 09:05:34 +0300

    Use admin layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('woo-order-dashboard::layouts.app')
+@extends('layouts.admin')
 
 @section('content')
 <div class="container mx-auto px-4 py-8">
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,4 +1,4 @@
-@extends('woo-order-dashboard::layouts.app')
+@extends('layouts.admin')
 
 @section('content')
 <div class="container-fluid">

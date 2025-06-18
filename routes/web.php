407b901544070663c181e144d6f88fb7246c39ac
makiomar <mo7amed.maki@gmail.com>
2025-06-19 01:23:04 +0300

    load orders controller from the package

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use Makiomar\WooOrderDashboard\Http\Controllers\WooOrderDashboardController;
+use Makiomar\WooOrderDashboard\Http\Controllers\OrdersController;
 
 Route::group(['middleware' => 'auth:admin'], function() {
     Route::get('/woo-orders', [WooOrderDashboardController::class, 'index'])->name('woo.dashboard');
     Route::get('/orders', [WooOrderDashboardController::class, 'orders'])->name('woo.orders');
     Route::get('/orders/{id}', [WooOrderDashboardController::class, 'show'])->name('woo.orders.show');
-    Route::get('/orders/create', 'OrdersController@create')->name('orders.create');
-    Route::get('/orders/products/search', 'OrdersController@productsSearch')->name('orders.products.search');
-    Route::get('/orders/customers/search', 'OrdersController@customersSearch')->name('orders.customers.search');
-    Route::post('/orders', 'OrdersController@store')->name('orders.store');
+    Route::get('/orders/create', [OrdersController::class, 'create'])->name('orders.create');
+    Route::get('/orders/products/search', [OrdersController::class, 'productsSearch'])->name('orders.products.search');
+    Route::get('/orders/customers/search', [OrdersController::class, 'customersSearch'])->name('orders.customers.search');
+    Route::post('/orders', [OrdersController::class, 'store'])->name('orders.store');
 }); 

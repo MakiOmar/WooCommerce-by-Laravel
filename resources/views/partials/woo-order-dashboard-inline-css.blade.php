022fab902dc79951b02a535f5fa66b220e71bd05
makiomar <mo7amed.maki@gmail.com>
2025-06-25 10:31:21 +0300

    Inline css fix

--- resources/views/partials/woo-order-dashboard-inline-css.blade.php
+++ resources/views/partials/woo-order-dashboard-inline-css.blade.php
@@ -1,3 +1,7 @@
+@php
+    $cssPath = base_path('vendor/makiomar/woo-order-dashboard/resources/assets/css/woo-order-dashboard.css');
+    $cssContent = file_exists($cssPath) ? file_get_contents($cssPath) : '/* CSS file not found */';
+@endphp
 <style>
-{!! file_get_contents(base_path('resources/assets/css/woo-order-dashboard.css')) !!}
+{!! $cssContent !!}
 </style> 
